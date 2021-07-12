b3910acf838f88928b8488cad7bcd41832ba297f
Maciej Kubicki <dev.kubicki@gmail.com>
2021-07-12 22:22:43 +0200

    V 1.1 Add needed file to run

---
+++ apiconnect_old.py
@@ -0,0 +1,42 @@
+import realmlist  # first realmlist execute then apiconnect !!!
+import wowapi
+import json
+import os
+import datetime
+
+key = '0d7282069bad432c8cac95125523f7cf'
+secretkey = 'wLcGwQgP72kzpMzH81I8siD4UbZTlH2f'
+
+api = wowapi.WowApi(key, secretkey)
+
+
+def current_date():
+    """ return date m/d/y/h/m/s """
+    current_date = datetime.datetime.now()
+    string_date = current_date.strftime("%m_%d_%Y_%H_%M_%S")
+    return string_date
+
+
+def save_file_json(file_to_save, id_group_server):
+    """ save downlaod file, taking arg id_group_server """
+    with open('ah_' + str(id_group_server) + '_' + current_date() + '.json', 'w') as json_file:
+        json.dump(file_to_save, json_file,) #indent=2)
+
+
+def auction_house_download(id_group_servers, region='eu', dynamic='dynamic-eu'):
+    """ send request to get auction house file, saveing it in Json with current date """
+
+    json_ah_file = api.get_auctions(region, dynamic, id_group_servers)
+    save_file_json(json_ah_file, id_group_servers)
+    print(current_date()+' pobrano auction house')
+    return json_ah_file
+
+def download_item_by_id(id_item):
+    """
+    function connect to blizzard server and download item_data and item_media where is icon of item
+    :param id_item:
+    :return: information about item, picture item
+    """
+    item_data = api.get_item_data('eu','static-eu', id_item)
+    item_media_data = api.get_item_media('eu','static-eu', id_item)
+    return item_data, item_media_data
---
+++ read_ah_file.py
@@ -0,0 +1,124 @@
+import realmlist as rl
+import item as sin
+import items as mul
+import csv
+"""
+ https://www.wowhead.com/item=123865   id=82800 = pet_cage
+ test['auctions'][0]['item']['id']
+"""
+file_name = 'data/Json/1084/ah_1084_07.06.2021_12;22.json'
+w1 = 'ah_1084_12_30_2020_17_10_47.json'
+file_open = rl.open_json_file(file_name) #taking name file
+
+list_single_class = []
+list_multi_class = [] # list with objects
+id_list = []
+
+def sort_by_id(list):
+    """
+    :param list:
+    :return: return json list file sorted by id
+    """
+    return list['item']['id']
+
+
+file = file_open['auctions']
+file.sort(key=sort_by_id)
+
+
+def add_to_multi(id_item):
+    mutli_object = mul.Items()
+    mutli_object.id_items = id_item
+    return mutli_object
+
+
+def add_to_single(row):
+    single_object = sin.Item()
+    selected_item = file[row]
+
+    single_object.id_item = selected_item['item']['id']
+    if 'unit_price' in file[row]:
+        single_object.price_unit = file[row]['unit_price']
+    elif 'buyout' in file[row]:
+            single_object.price_buyout = file[row]['buyout']
+    else:
+        single_object.price_buyout = file[row]['bid']
+
+
+    single_object.quantity = selected_item['quantity']
+    single_object.time_left = selected_item['time_left']
+
+    single_object.all_data_item = selected_item
+    list_single_class.append(single_object)
+    return single_object
+
+def get_from_csv_name_by_id(id):
+    """ function not used """
+
+    id_item = id
+    str_id_item = str(id_item)
+    with open('items.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['id'] == str_id_item:
+                return row['name item']
+
+def get_from_csv_id_by_name(name):
+    name_by = name
+    name_ = str(name_by)
+    with open('items.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if row['name item'] == name_:
+                return row['id']
+
+def get_info_from_multi_class_by_id(id):
+    """ find better algoritm serching
+    function return items object
+    """
+    for item in list_multi_class:
+        if item.id_items == id:
+            return item
+
+
+
+
+def create_dependency():
+    """
+    main function that is used to form classes item,items
+    taking from json parameters, and scraping website to get name of item
+    """
+
+    x = 0
+    y = 0
+    id = file[x]['item']['id']
+    id_list.append(file[x]['item']['id'])
+
+    object_items = add_to_multi(file[x]['item']['id'])
+    list_multi_class.append(object_items)
+    object_items.start()
+    #object_items.item_name = get_from_csv_name_by_id(object_items.id_items) bardzo nie optymalne
+
+    for i in range(len(file)):
+
+        if id == file[y]['item']['id']:
+            item = add_to_single(y)
+            object_items.list_item_class.append(item)
+            object_items.list_items.append(file[y])
+            y += 1
+
+        else:
+            x = y
+            object_items = add_to_multi(file[x]['item']['id'])
+            object_items.start()
+            #object_items.item_name = get_from_csv_name_by_id(object_items.id_items)
+            list_multi_class.append(object_items)
+            item = add_to_single(y)
+            object_items.list_item_class.append(item)
+            object_items.list_items.append(file[y])
+            id = file[y]['item']['id']
+            id_list.append(file[y]['item']['id'])
+            y += 1
+
+
+create_dependency()
